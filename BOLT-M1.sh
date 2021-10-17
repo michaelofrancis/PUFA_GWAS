d43b1ee173ec7e3bf8e7359c262faf7f389fae33
Mike Francis <51752343+michaelofrancis@users.noreply.github.com>
2021-10-17 17:35:51 -0400

    Update BOLT-M1.sh

--- BOLT-M1.sh
+++ BOLT-M1.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 #SBATCH --partition=highmem_p
-#SBATCH --job-name=BOLTM1
+#SBATCH --job-name=BOLTM1f
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=8
 #SBATCH --time=167:00:00
-#SBATCH --mem=600G
-#SBATCH --output=BOLTM1.%j.out
-#SBATCH --error=BOLTM1.%j.err
+#SBATCH --mem=300G
+#SBATCH --output=BOLTM1f.%j.out
+#SBATCH --error=BOLTM1f.%j.err
 #SBATCH --constraint=Intel
 
 cd /work/kylab/mike/PUFA-GWAS/BOLT-LMM/M1
@@ -15,7 +15,7 @@ cd /work/kylab/mike/PUFA-GWAS/BOLT-LMM/M1
 bgendir=("/scratch/mf91122/PUFA-GWAS/BOLT-LMM/genotypeQC")
 bfiledir=("/scratch/mf91122/PUFA-GWAS/Prune/4.bedfinal")
 phenodir=("/scratch/mf91122/PUFA-GWAS/pheno")
-outdir=("/scratch/mf91122/PUFA-GWAS/BOLT-LMM/results/M1")
+outdir=("/scratch/mf91122/PUFA-GWAS/BOLT-LMM/results/M1f")
 boltdir=("/home/mf91122/BOLT-LMM/BOLT-LMM_v2.3.4")
 
 #phenotypes=("RankTest" "RankbioavailableTest")
@@ -27,19 +27,22 @@ phenotypes=("w3FA_NMR_resinv" "w3FA_NMR_TFAP_resinv" "w6FA_NMR_resinv"
 		"MUFA_NMR_resinv" "MUFA_NMR_TFAP_resinv" "PUFA_MUFA_ratio_NMR_resinv")
 
 
-#pheno=("w3FA_NMR_resinv")
+pheno=("w3FA_NMR_resinv")
 
 m=1
 
 
-for j in ${phenotypes[@]} 
+for j in ${pheno[@]} 
 	do
 
 
 mkdir -p $outdir/$j
 
 $boltdir/bolt \
---bfile=$bfiledir/mergedbed \
+--bed=$bfiledir/chr{1..22}.bed \
+--bim=$bfiledir/chr{1..22}.bim \
+--fam=$bfiledir/chr1.fam \
+--geneticMapFile=$boltdir/tables/genetic_map_hg19_withX.txt.gz \
 --phenoFile="$phenodir"/PUFA_GWAS_pheno_M1.txt.pc.txt \
 --phenoCol="$j" \
 --covarFile="$phenodir"/PUFA_GWAS_pheno_M1.txt.pc.txt \
@@ -50,8 +53,8 @@ $boltdir/bolt \
 --qCovarCol=center{20:21} \
 --qCovarCol=Geno_batch \
 --covarCol=Sex \
---bgenFile="$bgendir"/chr{1..22}.bgen \
---sampleFile="$bgendir"/chr1.sample \
+--bgenFile="$bgendir"/chr{1..22}_b-keep.bgen \
+--sampleFile="$bgendir"/chr1_b-keep.sample \
 --lmm \
 --numThreads=16 \
 --LDscoresFile="$boltdir"/tables/LDSCORE.1000G_EUR.tab.gz \
