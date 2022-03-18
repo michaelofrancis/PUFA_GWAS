94276939685ecc6e3d6e8b918e344847066658f5
Mike Francis <51752343+michaelofrancis@users.noreply.github.com>
2022-03-18 16:45:35 -0400

    Update manqq.R

--- BOLT-LMM/manqq.R
+++ BOLT-LMM/manqq.R
@@ -7,7 +7,16 @@ phenotypes<-c("w3FA_NMR_resinv", "w3FA_NMR_TFAP_resinv", "w6FA_NMR_resinv",
 		"LA_NMR_TFAP_resinv", "PUFA_NMR_resinv", "PUFA_NMR_TFAP_resinv", 
 		"MUFA_NMR_resinv", "MUFA_NMR_TFAP_resinv", "PUFA_MUFA_ratio_NMR_resinv")
 
+phenotypenames<-c("Omega-3", "Omega-3 (TFAP)", "Omega-6" , "Omega-6 (TFAP)", 
+		"Omega-6:Omega-3 ratio", "DHA", "DHA (TFAP)", "LA", "LA (TFAP)",
+		"PUFAs", "PUFAs (TFAP)", "MUFAs", "MUFAs (TFAP)", "PUFAs:MUFAs ratio")
+
 m<-c(1,2)
+#m=2
+
+mancolors<-list(c("#96D4C1", "#699487"),
+		c("#0CEBA8", "#066C4E"))
+
 
 for (m in 1:length(m)){ #loop over models
 
@@ -15,6 +24,7 @@ dir<-paste("/scratch/mf91122/PUFA-GWAS/BOLT-LMM/results/M", m, "/", sep="")
 
 
 for (i in 1:length(phenotypes)){ #loop over phenotypes
+#m=1;i=1
 
 
 file<-read.table(paste(dir, phenotypes[i], "/BOLT1-statsFile-BgenSnps-m", m, sep=""), 
@@ -26,32 +36,36 @@ colnames(file2)[4]<-"P"
 #file3<-file%>%select(CHR, SNP, BP, P_BOLT_LMM_INF)
 #colnames(file3)[4]<-"P"
 
-suppressWarnings(dir.create(paste(dir, phenotypes[i], "/plot", sep="")))
+suppressWarnings(dir.create(paste("/scratch/mf91122/PUFA-GWAS/BOLT-LMM/results/plot/M", m, sep="")))
 
-plotoutputpath=paste(dir, phenotypes[i], "/plot/ManhattanBOLT-10242021-m", m, sep="")
-
+plotoutputpath=paste("/scratch/mf91122/PUFA-GWAS/BOLT-LMM/results/plot/M", m, "/",
+	phenotypes[i], "-Manhattan", sep="")
 
 #Make Manhattan plot P_BOLT_LMM
-png(filename=plotoutputpath, type="cairo")
+png(filename=plotoutputpath, type="cairo", width=1000,height=500)
 
+print(
 manhattan(file2,
-	main = paste(phenotypes[i], " BOLT-LMM UKB M", m, sep=""),
-	ylim = c(0, 30), cex = 0.6,
+	main = paste("UKB-EUR discovery ", "M", m, " ", phenotypenames[i], sep=""),
+	ylim = c(0, 300), cex = 0.6,
 	annotatePval = FALSE, annotateTop = FALSE,
-	cex.axis = 0.9, col = c('red', 'black'),
+	cex.axis = 0.9, 
+	col = mancolors[[m]],
 	suggestiveline = -log10(1e-05), genomewideline = -log10(5e-08),
-	chrlabs = (1:22)
+	chrlabs = as.character(1:22)
 	)#end manhattan
+)
 
 dev.off() 
 
 
-qqoutputpath=paste(dir, phenotypes[i], "/plot/QQBOLT-10242021-m", m, sep="")
+qqoutputpath=paste("/scratch/mf91122/PUFA-GWAS/BOLT-LMM/results/plot/M", m, "/", 
+        phenotypes[i], "-QQ", sep="")
 
-png(filename=qqoutputpath, type="cairo")
+png(filename=qqoutputpath, type="cairo", width=500,height=500)
 
 qq(file2$P,
-	main= paste(phenotypes[i], " BOLT-LMM UKB M", m, sep="")
+	main = paste("UKB-EUR discovery ", "M", m, " ", phenotypenames[i], sep="")
 	)
 
 dev.off()
