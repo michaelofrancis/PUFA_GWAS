dc0604b9b47ff129ea34631f037dbfc63415ff1e
Mike Francis <51752343+michaelofrancis@users.noreply.github.com>
2022-04-08 19:42:08 -0400

    Update munge.KET.R

--- munge/munge.KET.R
+++ munge/munge.KET.R
@@ -15,7 +15,7 @@ KET<-as_tibble(read.table(ketfile, header=T))
 KET2<-KET
 colnames(KET2)<-c("CHR","BP","ID","EFFECT_ALLELE","NON_EFFECT_ALLELE","EFFECT_ALLELE_FREQ","BETA","SE","P","NSTUDY","N")
 
-reformattedKET<-MungeSumstats::format_sumstats(path=KET2, ref_genome="GRCh37", INFO_filter=0.29)
+reformattedKET<-MungeSumstats::format_sumstats(path=KET2, ref_genome="GRCh37", INFO_filter=0.3)
 refoKET<-as_tibble(read.table(reformattedKET, header=T))
 
 write.table(refoKET, paste(dir, "/munge/Kettunen/", pheno[i],".txt", sep=""), quote=F, row.names=F)
